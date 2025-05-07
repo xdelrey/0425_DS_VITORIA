62330f059b5f7d757c9520ff4a190222690ea176
Xabi del Rey <xdelrey@gmail.com>
2025-05-07 09:18:17 +0200

    actualización juego hundir la flota

--- 1-Fundamentals/Python/Hundir_la_flota/main.py
+++ 1-Fundamentals/Python/Hundir_la_flota/main.py
@@ -1,10 +1,25 @@
 import utils
 import variables as vr
 
+#crear tablero
 tablero_jugador = utils.crear_tablero()
 tablero_rival = utils.crear_tablero()
-print(tablero_jugador)
 print("-"*10)
 
-tablero_modificado = utils.colocar_barco(tablero_jugador, vr.barco_jugador)
+#crear barcos
+barco_1 = crear_barco(2)
+barco_2 = crear_barco(2)
+barco_3 = crear_barco(2)
+barco_4 = crear_barco(3)
+barco_5 = crear_barco(3)
+barco_6 = crear_barco(4)
+
+tablero_modificado = utils.colocar_barco(vr.barco_jugador, tablero_jugador)
 print(tablero_jugador)
+
+print("-"*10)
+
+utils.disparar((0,2), tablero_modificado)
+print(tablero_jugador)
+
+utils.validar_casilla_inicial()
