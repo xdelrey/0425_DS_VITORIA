f5de4309befeef4d149057751a40152be3d8349e
Xabi del Rey <xdelrey@gmail.com>
2025-05-07 17:14:23 +0200

    avances en el juego de hundir la flota

--- 1-Fundamentals/Python/Hundir_la_flota/main.py
+++ 1-Fundamentals/Python/Hundir_la_flota/main.py
@@ -1,25 +1,30 @@
-import utils
+from utils import *
 import variables as vr
 
-#crear tablero
+#crear tableros vacíos
+#Tablero 1 Jugador (barcos Jug con disparos Rival)
+tablero_barcos_jugador = utils.crear_tablero()
+#Tablero 2 Jugador (disparos Jug)
+tablero_disparos_jugador
+#Tablero 1 Rival (barcos Rival con disparos Jug)
+tablero_barcos_rival
+#Tablero 2 Rival (disparos Rival)
+tablero_disparos_rival
+'''
 tablero_jugador = utils.crear_tablero()
 tablero_rival = utils.crear_tablero()
 print("-"*10)
 
-#crear barcos
-barco_1 = crear_barco(2)
-barco_2 = crear_barco(2)
-barco_3 = crear_barco(2)
-barco_4 = crear_barco(3)
-barco_5 = crear_barco(3)
-barco_6 = crear_barco(4)
-
-tablero_modificado = utils.colocar_barco(vr.barco_jugador, tablero_jugador)
+tablero_jugador = utils.colocar_barco(vr.barco_jugador, tablero_jugador)
 print(tablero_jugador)
+
+#tablero_rival = utils.colocar_barco(vr.barco_rival, tablero_rival)
+#print(tablero_rival)
 
 print("-"*10)
 
-utils.disparar((0,2), tablero_modificado)
+utils.disparar((0,2), tablero_jugador)
 print(tablero_jugador)
 
 utils.validar_casilla_inicial()
+'''
--- 1-Fundamentals/Python/Hundir_la_flota/utils.py
+++ 1-Fundamentals/Python/Hundir_la_flota/utils.py
@@ -4,18 +4,16 @@ def crear_tablero(x = 10):
     tablero = np.full((x, x), "_")
     return tablero
 
+########## NO NOS HACE FALTA ESTA FUNCIÓN ##############3
 def crear_barco(eslora):
     barco = np.full(eslora, "O")
     return (barco)
+########## NO NOS HACE FALTA ESTA FUNCIÓN ##############3
 
-def dibujar_barco(barco, tablero):
-    for i in barco:
-        for j in i:
-            tablero[j[0], j[1]] = 'O'
-    return(tablero)
-
-
-def disparar(casilla,tablero):
+def disparar(casilla,tablero = 10):
+    '''
+    función que recibe una casilla (un vector de 2 elementos) y un tablero (por defecto 10x10)
+    '''
     if tablero[casilla] in ('O', 'X'):
         tablero[casilla] = "X"
     elif tablero[casilla] in ("_", 'A'):
@@ -45,21 +43,24 @@ def validar_casilla_inicial(casilla, eslora, es_vertical, tablero = 10):
     return casilla_valida
 
 
-def colocar_barco (barco):
-    eslora = len(barco)
+def colocar_barco (eslora,tablero = 10):
+    '''
+    función que recibe la longitud de un barco y la dimensión de un tablero de juego (por defecto 10x10)
+    y devuelve una posición válida en la que se coloca el barco en vertical u horizontal, donde el barco no se saldrá del tablero
+    '''
     casilla_valida = False
     coordenadas = []
 
     # obtengo una casilla inicial y una dirección válidas para la longitud del barco
     while not casilla_valida:
-        casilla_inicial = np.random.randint(0, tamano_tablero, 2) # casilla inicial aleatoria
+        casilla_inicial = np.random.randint(0, tablero, 2) # casilla inicial aleatoria
         direccion = np.random.randint(0,2) # obtengo una dirección (horizontal o vertical) aleatoriamente
         # 0: horizontal, 1: vertical
-        casilla_valida = validar_casilla_inicial(casilla_inicial, eslora, direccion)
+        casilla_valida = validar_casilla_inicial(casilla_inicial, eslora, bool(direccion))
     #print("casilla inicial:", casilla_inicial, "vertical?", direccion, "eslora", eslora)
 
     # convierto la casilla inicial en la primera casilla del barco a colocar
-    coordenadas.append(casilla_inicial.tolist())
+    coordenadas.append(casilla_inicial)
     
     # calculo la localización del barco en el tablero
     fila_sgte = coordenadas[0][0]
@@ -77,4 +78,20 @@ def colocar_barco (barco):
     #print("después del for:", coordenadas)
 
     barco_colocado = np.array(coordenadas) # convierto la lista en un array de numpy
+
     return barco_colocado
+
+def crear_todos_los_barcos (lista_barcos_eslora):
+    todos_los_barcos = []
+
+    for num_barcos, eslora in lista_barcos_eslora:
+        for _ in range(num_barcos):
+            barco = colocar_barco(eslora)
+            todos_los_barcos.append(barco)
+    
+    return todos_los_barcos
+
+def dibujar_barco(barco, tablero):
+    for x, y in barco:
+        tablero[x, y] = 'O'
+    return tablero
